172b310ddd1bd7149e56973f2829b6b3215a4867
Jacob Swenson <jswens912@gmail.com>
2019-09-20 14:14:12 -0500

    linked list

---
+++ linked-lists/linked-lists.js
@@ -0,0 +1,8 @@
+'use strict';
+
+class LinkedList {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
---
+++ linked-lists/linked-lists.test.js
@@ -0,0 +1,8 @@
+const LinkedList = require('../linked-lists');
+
+
+describe('Linked lists test', () => {
+    it('creates a linked list object' () =>{
+
+    })
+})
