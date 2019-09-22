b68f3ce84f476b5a2064d03870253748b040b252
Jacob Swenson <jswens912@gmail.com>
2019-09-22 13:30:08 -0500

    finished linked lists

--- linked-lists/linked-lists.js
+++ linked-lists/linked-lists.js
@@ -1,8 +1,51 @@
 'use strict';
 
-class LinkedList {
+class Node
+{
   constructor(data) {
     this.data = data;
     this.next = null;
   }
 }
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  insert(data) {
+    let current = this.head;
+    let newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
+    current = newNode;
+    this.size++;
+  }
+
+  includes(value) {
+    let current = this.head;
+    while (current) {
+      console.log(current)
+      if (current.data === value) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  toString() {
+    let result = [];
+
+    let current = this.head;
+    while (current) {
+      result.push(current.data);
+      current = current.next;
+    }
+
+    return result.join(' ');
+  }
+}
+
+module.exports = LinkedList;
--- linked-lists/linked-lists.test.js
+++ linked-lists/linked-lists.test.js
@@ -1,8 +1,47 @@
-const LinkedList = require('../linked-lists');
+const LinkedList = require('./linked-lists');
 
+describe('LinkedList', () => {
+  let list = new LinkedList;
 
-describe('Linked lists test', () => {
-    it('creates a linked list object' () =>{
-
-    })
+  it('creates an empty linked list', () => {
+    expect(list).toEqual({ head: null, size: 0 });
+  })
 })
+
+describe('insert()', () => {
+  let list = new LinkedList;
+  list.insert(1);
+  list.insert(7);
+
+
+  it('adds an new node', () => {
+    expect(list.size).toEqual(2);
+    expect(list.head.data).toBe(7);
+  })
+  it('points head to first value', () => {
+    expect(list.head.data).toEqual(7);
+  })
+})
+
+describe('includes()', () => {
+  let list = new LinkedList;
+  list.insert(1);
+  list.insert(2);
+  list.insert(3);
+
+  it('returns true only if value exists', () => {
+    expect(list.includes(3)).toEqual(true);
+  })
+  it('returns false if value doesnt exist', () => {
+    expect(list.includes(5)).toEqual(false);
+  })
+})
+
+describe('toString()', () => {
+  let list = new LinkedList;
+  list.insert('hell?');
+  list.insert('Da');
+  it('returns all node values as a string', () => {
+    expect(list.toString()).toEqual('Da hell?');
+  })
+});
