7863a8e2c57de5290591868937aa52bc9d00bdbe
Jacob Swenson <jswens912@gmail.com>
2019-09-24 17:04:14 -0500

    just kidding travis dont hate me

--- code-challenges/arrayBinarySearch/binarySearch.js
+++ code-challenges/arrayBinarySearch/binarySearch.js
@@ -1,21 +1,18 @@
-'use strict'
+'use strict';
 
-
-function arrSearch(arr,val){
-
+function arrSearch(arr, val) {
   let low = arr[0];
-  let high = arr[arr.length-1]
+  let high = arr[arr.length - 1];
 
-  while(low <= high){
+  while (low <= high) {
     let mid = Math.floor(high + low / 2);
-    if(val<mid){
-      high = mid -1
-    }else if(val>mid){
-      low = mid + 1
-    }else{
-      return 2
+    if (val < mid) {
+      high = mid - 1;
+    } else if (val > mid) {
+      low = mid + 1;
+    } else {
+      return 2;
     }
-
   }
   return -1;
 }
--- code-challenges/arrayShift/arrayShift.js
+++ code-challenges/arrayShift/arrayShift.js
@@ -16,3 +16,5 @@ function shift(array, value) {
     }
   }
 }
+
+shift();
